2b95aa1b0843fa9fa7b6722eb95d603705b96995
Simon <simon.vergnaud@zenbus.fr>
2020-05-12 16:37:43 +0200

    [edit] add refresh (bugged)

--- vue.js
+++ vue.js
@@ -6,34 +6,41 @@ var app = new Vue({
     data: {
         tripUpdates: [],
         GTFS: GTFS,
-        selectedTrip: "",
+        selectedTrip: "-1",
         urlRT: null,
         rtSuccess: false,
-        GTFSSuccess: false
+        GTFSSuccess: false,
+        timer: "",
+        refresh: false
     },
-	mounted: function() {
-		
-    // Code that will run only after the
-    // entire view has been rendered
-			//	(function initMap(){
-		map = L.map('map').setView([51.505, -0.09], 13);
-		L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'}).addTo(map); 
-		//})();
-		
-	},
+    mounted: function () {
+
+        // Code that will run only after the
+        // entire view has been rendered
+        //	(function initMap(){
+        map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+        }).addTo(map);
+        //})();
+
+    },
+    created: function () {
+        setInterval(this.getNow, 1000);
+    },
     methods: {
         getETA: function (stopSequence) {
-            for (var i = 0; i < this.selectedTrip.tripUpdate.stopTimeUpdate.length; i++) {
-                if (stopSequence == this.selectedTrip.tripUpdate.stopTimeUpdate[i].stopSequence && stopSequence != 0) {
-                    return new Date(this.selectedTrip.tripUpdate.stopTimeUpdate[i].arrival.time * 1000).toLocaleTimeString();
+            for (var i = 0; i < this.tripUpdates[this.selectedTrip].tripUpdate.stopTimeUpdate.length; i++) {
+                if (stopSequence == this.tripUpdates[this.selectedTrip].tripUpdate.stopTimeUpdate[i].stopSequence && stopSequence != 0) {
+                    return new Date(this.tripUpdates[this.selectedTrip].tripUpdate.stopTimeUpdate[i].arrival.time * 1000).toLocaleTimeString();
                 }
             }
             return "N/A";
         },
         getETD: function (stopSequence) {
-            for (var i = 0; i < this.selectedTrip.tripUpdate.stopTimeUpdate.length; i++) {
-                if (stopSequence == this.selectedTrip.tripUpdate.stopTimeUpdate[i].stopSequence && stopSequence != 0) {
-                    return new Date(this.selectedTrip.tripUpdate.stopTimeUpdate[i].departure.time * 1000).toLocaleTimeString();
+            for (var i = 0; i < this.tripUpdates[this.selectedTrip].tripUpdate.stopTimeUpdate.length; i++) {
+                if (stopSequence == this.tripUpdates[this.selectedTrip].tripUpdate.stopTimeUpdate[i].stopSequence && stopSequence != 0) {
+                    return new Date(this.tripUpdates[this.selectedTrip].tripUpdate.stopTimeUpdate[i].departure.time * 1000).toLocaleTimeString();
                 }
             }
             return "N/A";
@@ -77,9 +84,8 @@ var app = new Vue({
                     var offsetEnd = 2;
 
                     var msg = AwesomeMessage.decode(new Uint8Array(xhr.response));
-
+                    //RT = [];
                     msg["entity"].forEach(function (entity, i) {
-
                         if (entity.id.includes("Vehicle")) {
                             msg["entity"][i + 1].tripUpdate.vehicle = entity.vehicle;
                             //displayVehiculeOnTrip(entity);
@@ -101,9 +107,11 @@ var app = new Vue({
                     //  displayTrips();
                 }
                 xhr.send(null);
-            })
-            this.tripUpdates = RT;
-            this.rtSuccess = true;
+            });
+            if(RT.length > 0){
+                this.tripUpdates = RT;
+                this.rtSuccess = true;
+            }
         },
         allowDrop: function (e) {
             e.preventDefault();
@@ -118,12 +126,22 @@ var app = new Vue({
         setGTFSSuccess: function () {
             this.GTFSSuccess = true;
         },
-        displayVehiculeOnTrip: displayVehiculeOnTrip
+        displayVehiculeOnTrip: displayVehiculeOnTrip,
+        getNow: function () {
+            var today = new Date();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            this.timer = time;
+        }
     },
     watch: {
         // à chaque fois que le selectedTrip change, cette fonction s'exécutera
         selectedTrip: function (newSelectedTrip, oldSelectedTrip) {
             this.displayVehiculeOnTrip();
+        },
+        refresh: function(newRefresh, oldRefresh){
+            this.rtSuccess = false;
+            RT = []
+            setInterval(this.runRT(this.urlRT), 5000);
         }
     }
 });
